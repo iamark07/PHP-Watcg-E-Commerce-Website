48cca80790e66c493f37f2d37104ca2e9f8eafad
iamark07 <arbazkarimi@gmail.com>
2024-07-04 16:17:44 +0530

    update

---
+++ assets/js/register.js
@@ -0,0 +1,6 @@
+// show and hide register form
+
+function show_reg_form(){
+    document.querySelector(".reg_form").classList.add("show_register_form");
+    // document.querySelector(".creat_account").classList.add("hide_creat_account");
+}
