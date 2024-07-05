78337393ed8baabacf01032da8ddaee9b711c65f
iamark07 <arbazkarimi@gmail.com>
2024-07-05 13:51:07 +0530

    update

--- assets/js/register.js
+++ assets/js/register.js
@@ -10,16 +10,24 @@ function close_register_form(){
     document.querySelector(".creat_account").classList.remove("hide_creat_account");
 }
 
+// for hide show password
 
+document.querySelectorAll(".pass_icon_show").forEach(show_pass =>{
+    show_pass.addEventListener("click", ()=>{
+        show_pass.classList.add("hide_pass_icon");
+        let input_pass = show_pass.previousElementSibling;
+        let pass_icon_hide = show_pass.nextElementSibling;
+        pass_icon_hide.classList.add("show_pass");
+        input_pass.setAttribute("type", "text");
+    });
+});
 
-function show_pass(){
-    document.querySelector("#pass").setAttribute("type", "text");
-    document.querySelector(".pass_icon_show").classList.add("hide_pass_icon");
-    document.querySelector(".pass_icon_hide").classList.add("show_pass");
-}
-
-function hide_pass(){
-    document.querySelector("#pass").setAttribute("type", "password");
-    document.querySelector(".pass_icon_show").classList.remove("hide_pass_icon");
-    document.querySelector(".pass_icon_hide").classList.remove("show_pass");
-}
+document.querySelectorAll(".pass_icon_hide").forEach(hide_pass =>{
+    hide_pass.addEventListener("click", ()=>{
+        let show_pass_icon = hide_pass.previousElementSibling;
+        show_pass_icon.classList.remove("hide_pass_icon");
+        let input_pass = hide_pass.previousElementSibling.previousElementSibling;
+        hide_pass.classList.remove("show_pass");
+        input_pass.setAttribute("type", "password");
+    });
+});
