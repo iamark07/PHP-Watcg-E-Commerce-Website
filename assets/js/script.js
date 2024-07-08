37ac20dcf8ac57fbda7ba03f40fd8ad275831faf
iamark07 <arbazkarimi@gmail.com>
2024-07-08 14:10:11 +0530

    menu slider update

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,22 +21,22 @@ mob_search_btn.addEventListener("click", ()=>{
 let open_menu_btn = document.getElementById("open_menu_btn");
 
 open_menu_btn.addEventListener("click" , ()=> {
-    document.querySelector(".menu_slider").classList.add("open_menu_active");
+    document.querySelector(".menu_slider").classList.toggle("open_menu_active");
 })
 
 // close menu function
 
-let close_menu_btn = document.getElementById("close_menu_btn");
+// let close_menu_btn = document.getElementById("close_menu_btn");
 
-close_menu_btn.addEventListener("click" , ()=> {
-    document.querySelector(".menu_slider").classList.remove("open_menu_active");
-})
+// close_menu_btn.addEventListener("click" , ()=> {
+//     document.querySelector(".menu_slider").classList.remove("open_menu_active");
+// })
 let show_all_menu_icon = document.querySelector(".show_all_menu_icon");
 
-show_all_menu_icon.addEventListener("click" , ()=> {
-    document.querySelector(".mob_all_nav_show").classList.toggle("all_nav_show_hide");
+show_all_menu_icon.addEventListener("click", ()=> {
+    // console.log("hello");
+    document.querySelector(".mob_nav ul").classList.toggle("all_nav_show_hide");
     document.querySelector(".show_all_menu_icon").classList.toggle("show_all_menu_active");
-    document.querySelector(".mob_nav_container").classList.toggle("mob_nav_container_active");
 });
 
 // show all filter content
